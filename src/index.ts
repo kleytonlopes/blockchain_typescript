da69aca25220fe7c91a475b4ea821c93320c1d99
Kleyton Lopes <kleyton.lop@gmail.com>
2021-09-07 20:50:14 -0300

    feat: create Blockchain and add two blocks

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,8 @@
-"use strict";
-console.log('Hello world!');
+import Block from './Model/Block'
+import Blockchain from './Model/Blockchain'
+
+let myCoin = new Blockchain()
+myCoin.addBlock(new Block(1, '654321', {amount: 4}));
+myCoin.addBlock(new Block(2, '789123', {amount: 10}));
+
+console.log(JSON.stringify(myCoin, null, 4));
